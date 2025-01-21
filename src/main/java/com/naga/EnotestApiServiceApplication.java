61df8090f136b4e7dfcdbcf04d0e79901f6c5e20
gndr3540 <dastagirireddy3540@gmail.com>
2025-01-21 09:00:37 +0530

    initial files

---
+++ src/main/java/com/naga/EnotestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.naga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotestApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotestApiServiceApplication.class, args);
+	}
+
+}
